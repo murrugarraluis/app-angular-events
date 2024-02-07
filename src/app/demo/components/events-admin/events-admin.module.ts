29d6ff0ae7612be0d329b377df2b4fd171f7a02f
Luis Murrugarra <luismurrugarra17@gmail.com>
2024-02-07 18:38:26 -0500

    feat: Layout Crud

--- src/app/demo/components/events-admin/events-admin.module.ts
+++ src/app/demo/components/events-admin/events-admin.module.ts
@@ -2,6 +2,21 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EventsAdminRountingModule} from "./events-admin-rounting.module";
 import {EventCrudPageComponent} from "./pages/event-crud-page/event-crud-page.component";
+import {ButtonModule} from "primeng/button";
+import {DialogModule} from "primeng/dialog";
+import {DropdownModule} from "primeng/dropdown";
+import {FileUploadModule} from "primeng/fileupload";
+import {FormsModule} from "@angular/forms";
+import {InputNumberModule} from "primeng/inputnumber";
+import {InputTextModule} from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {RatingModule} from "primeng/rating";
+import {RippleModule} from "primeng/ripple";
+import {SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {ToastModule} from "primeng/toast";
+import {ToolbarModule} from "primeng/toolbar";
 
 
 @NgModule({
@@ -11,6 +26,21 @@ import {EventCrudPageComponent} from "./pages/event-crud-page/event-crud-page.co
     imports: [
         CommonModule,
         EventsAdminRountingModule,
+        ButtonModule,
+        DialogModule,
+        DropdownModule,
+        FileUploadModule,
+        FormsModule,
+        InputNumberModule,
+        InputTextModule,
+        InputTextareaModule,
+        RadioButtonModule,
+        RatingModule,
+        RippleModule,
+        SharedModule,
+        TableModule,
+        ToastModule,
+        ToolbarModule,
     ]
 })
 export class EventsAdminModuleModule {
--- src/app/demo/components/pages/crud/crud-routing.module.ts
+++ src/app/demo/components/pages/crud/crud-routing.module.ts
@@ -1,3 +1,5 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CrudComponent } from './crud.component';
